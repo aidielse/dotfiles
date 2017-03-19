7247b9770f869a66dfb629d9303cd15a98e39447
aidielse <aarons104@gmail.com>
2017-03-19 14:36:32 -0400

    moved setup.sh to a makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+init:
+    sudo apt-get -y install gdb virtualenv virtualenvwrapper
+    wget -q -O- https://github.com/hugsy/gef/raw/master/gef.sh | sh
+
--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt-get -y install gdb virtualenv virtualenvwrapper
-
-wget -q -O- https://github.com/hugsy/gef/raw/master/gef.sh | sh
